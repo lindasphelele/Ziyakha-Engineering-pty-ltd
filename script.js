24790bb708e85772fdc5e9c2f6bea3ef3504cfb7
lindasphelele <106629213+lindasphelele@users.noreply.github.com>
2026-01-19 13:53:58 +0200

    Remove contact form submission alert

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Thank you for contacting Ziyakha Engineering. We will get back to you shortly.');
-});
+
