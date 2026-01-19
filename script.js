a73b51c1d499e0b12e4bdaab0ce9ea927ff0ed5b
lindasphelele <106629213+lindasphelele@users.noreply.github.com>
2026-01-19 10:51:32 +0200

    Add form submission handler with alert message

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for contacting Ziyakha Engineering. We will get back to you shortly.');
+});
