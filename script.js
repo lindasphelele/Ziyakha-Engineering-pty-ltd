010c6188c88750303f86365f08e63578fc40ebe6
lindasphelele <106629213+lindasphelele@users.noreply.github.com>
2026-01-22 09:54:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,7 @@ function toggleMenu() {
     navLinks.classList.toggle('active');
 }
 
-// Close the menu if a user clicks outside of it
-window.addEventListener('click', function(e) {
-    const navLinks = document.getElementById('navLinks');
-    const menuIcon = document.querySelector('.menu-icon');
-    
-    if (navLinks.classList.contains('active') && 
-        !navLinks.contains(e.target) && 
-        !menuIcon.contains(e.target)) {
-        navLinks.classList.remove('active');
-    }
-});
-
-// Ensure every time a "page" is clicked, we start at the top
+// Ensure "page" starts at the top when switching
 document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', () => {
         window.scrollTo(0, 0);
